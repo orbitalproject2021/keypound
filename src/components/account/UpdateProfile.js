5524599a21056da1a3a5455e274498731a493bdc
Ivan <77185900+wpinrui@users.noreply.github.com>
2021-05-28 23:19:21 +0800

    Bugfix: UpdateProfile
    Fixed (yet another :-1: ) bug in which changing the user's password would cause an error to be shown due to the user being logged out momentarily

--- src/components/account/UpdateProfile.js
+++ src/components/account/UpdateProfile.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Form, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import useAuthForm from "./utility/useAuthForm";
+import {
+    Email,
+    Password,
+    PasswordConfirm,
+    Submit,
+    authStyle,
+    Message,
+} from "./utility/AuthSheets";
 
 export default function UpdateProfile() {
     const {
@@ -18,13 +26,21 @@ export default function UpdateProfile() {
         loading,
         setLoading,
     } = useAuthForm();
+    const successMsg = "Successfully updated profile.";
 
     useEffect(() => {
-        !currentUser && setError("Error: You must be logged in.");
+        !currentUser &&
+            setError((prev) =>
+                prev === successMsg ? prev : "Error: You must be logged in."
+            );
     }, [currentUser, setError]);
 
     function handleSubmit(e) {
         e.preventDefault();
+        if (!currentUser) {
+            return;
+        }
+
         setMessage("");
         setError("");
 
@@ -47,173 +63,39 @@ export default function UpdateProfile() {
             .catch((error) => setError(error.message))
             .finally(() => setLoading(false));
     }
+
+    const placeholder = "Leave blank to keep the same.";
     return (
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Update Email</h2>
-                    {message && <Alert variant="success">{message}</Alert>}
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form
-                        onSubmit={
-                            currentUser
-                                ? handleSubmit
-                                : (e) => {
-                                      e.preventDefault();
-                                  }
-                        }
-                    >
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                type="email"
-                                ref={emailRef}
-                                defaultValue={currentUser && currentUser.email}
-                                required
-                            />
-                        </Form.Group>
+                    <h2 className={authStyle.title}>Update Email</h2>
+                    <Message error={error} message={message} />
+                    <Form onSubmit={handleSubmit}>
+                        <Email
+                            reference={emailRef}
+                            defaultValue={currentUser && currentUser.email}
+                            required={true}
+                            onChange={() => setMessage("")}
+                        />
+                        <Password
+                            reference={passwordRef}
+                            placeholder={placeholder}
+                            onChange={() => setMessage("")}
+                        />
 
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                ref={passwordRef}
-                                placeholder="Leave blank to keep the same"
-                            />
-                        </Form.Group>
-
-                        <Form.Group id="passwordConfirm">
-                            <Form.Label>Password Confirm</Form.Label>
-                            <Form.Control
-                                type="password"
-                                ref={passwordConfirmRef}
-                                placeholder="Leave blank to keep the same"
-                            />
-                        </Form.Group>
-
-                        <div style={{ padding: "10pt" }}></div>
-                        <Button
-                            disabled={loading}
-                            className="w-100"
-                            type="submit"
-                        >
-                            Update
-                        </Button>
+                        <PasswordConfirm
+                            reference={passwordConfirmRef}
+                            placeholder={placeholder}
+                            onChange={() => setMessage("")}
+                        />
+                        <Submit loading={loading}>Update</Submit>
                     </Form>
                 </Card.Body>
             </Card>
-            <div className="w-100 text-center mt-2">
+            <div className={authStyle.link}>
                 <Link to="/">Back to Home</Link>
             </div>
         </>
     );
 }
-
-/*
-export default function UpdateProfile() {
-    const {
-        emailRef,
-        passwordRef,
-        passwordConfirmRef,
-        updateEmail,
-        updatePassword,
-        currentUser,
-        error,
-        setError,
-        message,
-        setMessage,
-        loading,
-        setLoading,
-    } = useAuthForm();
-
-    useEffect(() => {
-        !currentUser && setError("Error: You must be logged in.");
-    }, [currentUser]);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        setMessage("");
-        setError("");
-
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError("Passwords do not match.");
-        }
-
-        const promises = [];
-        if (emailRef.current.value !== currentUser.email) {
-            promises.push(updateEmail(emailRef.current.value));
-        }
-        if (passwordRef.current.value) {
-            promises.push(updatePassword(passwordRef.current.value));
-        }
-
-        Promise.all(promises)
-            .then(() => {
-                setMessage("Successfully updated profile.");
-            })
-            .catch((error) => setError(error.message))
-            .finally(() => setLoading(false));
-    }
-    return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Update Email</h2>
-                    {message && <Alert variant="success">{message}</Alert>}
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form
-                        onSubmit={
-                            currentUser
-                                ? handleSubmit
-                                : (e) => {
-                                      e.preventDefault();
-                                  }
-                        }
-                    >
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                type="email"
-                                ref={emailRef}
-                                defaultValue={currentUser && currentUser.email}
-                                required
-                            />
-                        </Form.Group>
-
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                ref={passwordRef}
-                                placeholder="Leave blank to keep the same"
-                            />
-                        </Form.Group>
-
-                        <Form.Group id="passwordConfirm">
-                            <Form.Label>Password Confirm</Form.Label>
-                            <Form.Control
-                                type="password"
-                                ref={passwordConfirmRef}
-                                placeholder="Leave blank to keep the same"
-                            />
-                        </Form.Group>
-
-                        <div style={{ padding: "10pt" }}></div>
-                        <Button
-                            disabled={loading}
-                            className="w-100"
-                            type="submit"
-                        >
-                            Update
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
-                <Link to="/">Back to Home</Link>
-            </div>
-        </>
-    );
-}
-
-*/
