28b22897648cce6beb1acacdded8acb9532ca173
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2021-06-04 11:53:11 +0800

    Add Goals Frontend
    Added a form to receive financial goals from users

--- src/components/pages/Goals.js
+++ src/components/pages/Goals.js
@@ -1,12 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Form } from "react-bootstrap";
+import { ContentCard, Content } from "../ContentCard";
 import Navigation from "../Navigation";
 
 function Goals() {
-    useEffect(() => {
-        document.title = "Goals - Spendee";
-    }, []);
+  const typeRef = useRef();
+  const amountRef = useRef();
+  const motivationsRef = useRef();
 
-    return <Navigation active="goals" />;
+  useEffect(() => {
+    document.title = "Goals - Spendee";
+  }, []);
+
+  return (
+    <>
+      <Navigation active="goals" />;
+      <ContentCard>
+        <Content area={[1, 3, 1, 3]} title="Add goals">
+          <p>Add Goals Here</p>
+          <Form>
+            <Form.Group id="type">
+              <Form.Label>Type</Form.Label>
+              <Form.Control type="text" step="any" ref={typeRef} required />
+            </Form.Group>
+
+            <Form.Group id="amount">
+              <Form.Label>Amount</Form.Label>
+              <Form.Control
+                type="number"
+                step="any"
+                ref={amountRef}
+                min="0.01"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group id="motivations">
+              <Form.Label>Motivations</Form.Label>
+              <Form.Control
+                type="text"
+                step="any"
+                ref={motivationsRef}
+                required
+              />
+            </Form.Group>
+          </Form>
+        </Content>
+      </ContentCard>
+    </>
+  );
 }
 
 export default Goals;
