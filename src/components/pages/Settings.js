2a2947153d05449ae7f7ca0083482e2574db0a4b
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2021-06-27 13:17:57 +0800

    Frontend added
    Added ContentCard, form , income field

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -1,12 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navigation from "../Navigation";
+import { ContentCard, Content } from "../ContentCard";
+import { Alert, Form, Button, Dropdown, DropdownButton } from "react-bootstrap";
+import { db } from "../../firebase";
+import { useAuth } from "../../contexts/AuthContext";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 function Settings() {
-    useEffect(() => {
-        document.title = "Settings - Spendee";
-    }, []);
+  const incomeRef = useRef();
 
-    return <Navigation active="settings" />;
+  useEffect(() => {
+    document.title = "Settings - Spendee";
+  }, []);
+
+  return (
+    <>
+      <Navigation active="settings" />;
+      <ContentCard>
+        <Content title="update income">
+          <p>Update Income here.</p>
+          <Form>
+            <Form.Group id="income">
+              <Form.Label>Income</Form.Label>
+              <Form.Control
+                type="number"
+                step="any"
+                ref={incomeRef}
+                min="0.01"
+              ></Form.Control>
+            </Form.Group>
+          </Form>
+        </Content>
+      </ContentCard>
+    </>
+  );
 }
 
 export default Settings;
