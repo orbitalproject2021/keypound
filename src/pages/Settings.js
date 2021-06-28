af392ebf3a52c6653203c6ea2e27cc9a63f95622
Ivan <77185900+wpinrui@users.noreply.github.com>
2021-06-28 20:23:39 +0800

    Manual Merge of yq changes
    Catch blocks in AddTransaction, Settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,12 +1,83 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navigation from "../components/Navigation";
+import { ContentCard, Content } from "../components/ContentCard";
+import { Form, Button } from "react-bootstrap";
+import { db } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
+import "firebase/auth";
+import "firebase/firestore";
 
 function Settings() {
+    const incomeRef = useRef();
+
+    const [disabled, setDisabled] = useState(false);
+    const [message, setMessage] = useState("");
+    const { currentUser } = useAuth();
+
     useEffect(() => {
         document.title = "Settings - Spendee";
+        incomeRef.current.focus();
     }, []);
 
-    return <Navigation active="settings" />;
+    const handlesubmit = (e) => {
+        setDisabled(true);
+        e.preventDefault();
+
+        // reference to user document
+        var docRef = db.collection("users").doc(currentUser.uid);
+
+        docRef.get().then((doc) => {
+            const monthArr = doc.data().monthArr;
+            const index = monthArr.length - 1;
+            const newIncome = incomeRef.current.value;
+            monthArr[index].income = newIncome;
+            docRef
+                .update({
+                    monthArr: monthArr,
+                })
+                .then(() => {
+                    setMessage("Income updated successfully");
+                });
+        });
+    };
+    return (
+        <>
+            <Navigation active="settings" />
+            <ContentCard>
+                <Content title="update income">
+                    <p>Update your income here.</p>
+                    <Form onSubmit={handlesubmit}>
+                        <Form.Group id="income">
+                            <Form.Label>Income</Form.Label>
+                            <Form.Control
+                                type="number"
+                                step="any"
+                                ref={incomeRef}
+                                min="0.01"
+                                onChange={() => {
+                                    setDisabled(false);
+                                    setMessage("");
+                                }}
+                            ></Form.Control>
+                        </Form.Group>
+                        <div style={{ padding: "10pt" }}></div>
+                        <Button
+                            disabled={disabled}
+                            type={"submit"}
+                            className={"custom-button"}
+                        >
+                            Update
+                        </Button>
+                        {message && (
+                            <>
+                                <span className="custom-alert">{message}</span>
+                            </>
+                        )}
+                    </Form>
+                </Content>
+            </ContentCard>
+        </>
+    );
 }
 
 export default Settings;
